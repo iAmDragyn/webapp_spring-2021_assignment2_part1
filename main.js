54e9c873fee9ff2e3c803a14bf40e562ba4ef0fa
Alex Verkest <31321596+iAmDragyn@users.noreply.github.com>
2021-03-15 16:08:35 -0600

    Pages Movin'

--- main.js
+++ main.js
@@ -1,31 +1,39 @@
-var startingBook = 0;
-
 const app = Vue.createApp({
     data() {
         return {
             keyword: '',
             result: null,
+            startingBook: 0,
         }
     },
     methods: {
         searchGoogleBooks() {
-            fetch('https://www.googleapis.com/books/v1/volumes?q=' + this.keyword + '&startIndex=' + startingBook + '&maxResults=30')
+            console.log(this.startingBook);
+            fetch('https://www.googleapis.com/books/v1/volumes?q=' + this.keyword + '&startIndex=' + this.startingBook + '&maxResults=30')
                 .then(response => response.json())
                 .then(json => this.result = json);
             document.querySelector("#paginationButtons").classList.remove("invisible");
-        }
+        },
+        nextBook() {
+            this.startingBook += 30;
+            this.searchGoogleBooks();
+            console.log(this.startingBook);
+            if (this.startingBook > 0) {
+                document.querySelector("#previousButton").classList.remove("invisible");
+            }
+            this.$forceUpdate();
+        },
+        previousBook() {
+            this.startingBook -= 30;
+            this.searchGoogleBooks();
+            console.log(this.startingBook);
+            if (this.startingBook < 30) {
+                document.querySelector("#paginationButtons").classList.add("invisible");
+            }
+            this.$forceUpdate();
+        },
     }
 })
-
-function nextPage() {
-    startingBook += 30;
-    this.app.searchGoogleBooks;
-    this.app.template
-}
-
-function previousPage() {
-    startingBook -= 30;
-}
 
 
 
