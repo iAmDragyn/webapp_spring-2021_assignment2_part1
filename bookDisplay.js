dd5ec93a371e9f73e93375ce359a06e6ac993292
Tammy Husnetdinova <43862224+t-husnetdinova@users.noreply.github.com>
2021-03-17 21:57:26 -0600

    fixed author display formatting

--- bookDisplay.js
+++ bookDisplay.js
@@ -11,7 +11,7 @@ app.component('book-display', {
         `<ul class="col-sm-12 col-md-6 col-xl-4">
             <a class="list-group-item list-group-item-action list-group-item-dark" :href="this.bookObj.selfLink"> {{ this.bookObj.volumeInfo.title }} </a>
             <img v-if="this.bookObj.volumeInfo.imageLinks.thumbnail != undefined" :src="this.bookObj.volumeInfo.imageLinks.thumbnail" width="150" height="150">
-            <li class= "list-group-item" > {{ this.bookObj.volumeInfo.authors }} </li>
+            <li class= "list-group-item" v-if="this.bookObj.volumeInfo.authors[0] !=undefined"> {{ this.bookObj.volumeInfo.authors[0] }} </li>
             <li class= "list-group-item" v-if="this.bookObj.volumeInfo.mainCategory !=undefined"> {{ this.bookObj.volumeInfo.mainCategory }} </li>
             <li class= "list-group-item" v-if="this.bookObj.volumeInfo.description !=undefined"> {{ this.bookObj.volumeInfo.description }} </li>
             <li class= "list-group-item" v-if="this.bookObj.volumeInfo.publisher != undefined"> {{ this.bookObj.volumeInfo.publisher }} </li>
